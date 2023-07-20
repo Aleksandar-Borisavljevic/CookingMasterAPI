25a16f88ba18f9e7c492ee770aac0cc9340d4979
faceless <faceless@DESKTOP-IN9IRUO>
2023-07-20 18:44:44 +0200

    Add project files.

---
+++ CookingMasterAPI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookingMasterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public int ID { get; set; }
+
+        [HttpGet("RandomNumber/{ID}")]
+        public int ReturnRandomNumber()
+        {
+            ID = 5;
+            Random rnd = new Random();
+            return rnd.Next(0, 100);
+        }
+
+        [HttpGet("RandomNumber")]
+        public int VratiRandomBroj()
+        {            
+            Random rnd = new Random();
+            return rnd.Next(0, 100);
+        }
+
+        [HttpPost]
+
+        public int SumirajDvaBroja(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
