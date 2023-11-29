8e14e1752a15b5b0f01bcfe5815d5d7d2bfac09b
Petar Stošić <petar.stosic.itransport@gmail.com>
2023-11-29 20:29:03 +0100

    CulinaryRecipes

---
+++ src/Application/CulinaryRecipes/Commands/Create/CreateCulinaryRecipeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CookingMasterApi.Application.Common.Interfaces;
+using CookingMasterApi.Application.IngredientCategories.Commands.Create;
+using CookingMasterApi.Domain.Entities;
+using MediatR;
+
+namespace CookingMasterApi.Application.CulinaryRecipes.Commands.Create;
+public class CreateCulinaryRecipeCommandHandler : IRequestHandler<CreateCulinaryRecipeCommand, CreateCulinaryRecipeCommandResult>
+{
+    private readonly ICookingMasterDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreateCulinaryRecipeCommandHandler(ICookingMasterDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<CreateCulinaryRecipeCommandResult> Handle(CreateCulinaryRecipeCommand request, CancellationToken cancellationToken)
+    {
+        var entity = _mapper.Map<CulinaryRecipe>(request);
+        var result = (await _context.CulinaryRecipes.AddAsync(entity)).Entity;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new CreateCulinaryRecipeCommandResult(result.RecipeName, result.RecipeDescription, result.CuisineType);
+    }
+}
---
+++ src/WebUI/Controllers/CulinaryRecipeController.cs
@@ -0,0 +1,14 @@
+﻿using CookingMasterApi.Application.CulinaryRecipes.Commands.Create;
+using CookingMasterApi.Application.IngredientCategories.Commands.Create;
+using CookingMasterApi.WebUI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+public class CulinaryRecipeController : ApiControllerBase
+{
+    [HttpPost(nameof(CreateCulinaryRecipe))]
+    public async Task<ActionResult<CreateCulinaryRecipeCommandResult>> CreateCulinaryRecipe(CreateCulinaryRecipeCommand command)
+    {
+        return Ok(await Mediator.Send(command));
+    }
+}
